df8904a61d1e13e11b7fdffdc2c2e37f0a5eaefd
tudinkojhb024 <tumisomphahle14@gmail.com>
2025-10-19 01:53:53 +0200

    Added notebook for data exploration

--- scripts/extract.py
+++ scripts/extract.py
@@ -5,6 +5,9 @@ def extract_data(path:str) -> pd.DataFrame:
     print("Extract data from CSV...")
 
     df = pd.read_csv(path)
+    df = df.dropna(subset=["country", "points", "price"])
+    df["price"] = df["price"].astype(float)
+    df["points"] = df["points"].astype(float)
 
     return df
 
